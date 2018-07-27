11eb3c05c63fc1ac22de1d578040026950f83a54
susmitha1298 <41729009+susmitha1298@users.noreply.github.com>
2018-07-27 14:52:35 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,13 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner reader = new Scanner(System.in);
+	System.out.println("Enter a number:");
+	int num=reader.nextInt( );
+	if(num % 2==0)
+	System.out.println("Even");
+	else
+	System.out.println("Odd");
+	}
+}
