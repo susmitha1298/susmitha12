28985c5f0e66113058bd1efc1ba4dcaf143c4d93
susmitha1298 <41729009+susmitha1298@users.noreply.github.com>
2018-07-27 15:08:04 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,9 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	char ch ='*';
+	if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+	System.out.println("Alphabet");
+	else
+	System.out.println("Not");
