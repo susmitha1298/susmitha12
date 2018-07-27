d6558d5a5bd50720e941010575c90193f9647cd0
susmitha1298 <41729009+susmitha1298@users.noreply.github.com>
2018-07-27 14:41:49 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,16 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int n;	
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter a number");
+n=sc.nextInt( );
+if(n>0)
+System.out.println("Positive");
+else if(n<0)
+System.out.println("Negative");
+else
+System.out.println("Zero");
+	}
+}
