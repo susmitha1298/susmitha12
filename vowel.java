b9500f4cbb2de58f9878d80ecaec46e3ce700159
susmitha1298 <41729009+susmitha1298@users.noreply.github.com>
2018-07-27 15:02:57 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,11 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	char ch ='a';
+	if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+	System.out.println("Vowel");
+	else
+	System.out.println("Constant");
+	}
+}
