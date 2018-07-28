5c5e76e99f072c484b8ab2a633693a22951055fe
susmitha1298 <41729009+susmitha1298@users.noreply.github.com>
2018-07-28 08:38:04 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,15 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 int num = 100, sum = 0;
+
+        for(int i = 1; i <= num; ++i)
+        {
+            // sum = sum + i;
+            sum += i;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
