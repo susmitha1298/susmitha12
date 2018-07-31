def6842290260648ebb8523abc6cad5ad45805ae
susmitha1298 <41729009+susmitha1298@users.noreply.github.com>
2018-07-31 12:23:31 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Prime
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+int num,i=2;
+Scanner sc=new Scanner(System.in);
+num=sc.nextInt();
+if(i==2)
+{
+	System.out.println("yes");
+}
+ else if(num%i==0)
+	{
+		
+		System.out.println(" no");
+}
+else
+{
+			System.out.println("yes");
+}
+}
+}
