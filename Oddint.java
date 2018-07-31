130b23f4b31c4fd397f3cc46d0ea555517a50afe
susmitha1298 <41729009+susmitha1298@users.noreply.github.com>
2018-07-31 12:26:44 +0530

    Create Oddint.java

---
+++ Oddint.java
@@ -0,0 +1,19 @@
+class Oddint
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int num,m,n;
+	Scanner sc=new Scanner(System.in);
+	m=sc.nextInt();
+	n=sc.nextInt();
+for(num=m;num<=n;num++)
+{
+	if(num%2==1)
+	{
+		System.out.println("number is" +num);
+	}
+// your code goes here
+}
+}
+}
+
