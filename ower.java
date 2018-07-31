ba090e25c4f10b9643463e050770b8938d67249e
susmitha1298 <41729009+susmitha1298@users.noreply.github.com>
2018-07-31 12:01:40 +0530

    Create ower.java

---
+++ ower.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+//* Name of the class has to be "Main" only if the class is public. 
+        class ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+
+   
+
+        int base, exponent ;
+        Scanner sc=new Scanner(System.in);
+        base= sc.nextInt();
+        exponent=sc.nextInt();
+        double result = Math.pow(base, exponent);
+
+        System.out.println("Answer = " + result);// your code goes here
+	}
+}
+	
