111faa7412a4c34e6d1d1e185ff664c696ebb1bc
susmitha1298 <41729009+susmitha1298@users.noreply.github.com>
2018-08-03 13:48:45 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,12 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("The Odd Numbers are:");
+		for (int i = 1; i <= 10; i++) {
+			if (i % 2 != 0) {
+				System.out.print(i + " ");
+			}
+		}
+	}
+}
